6e51fb07335927a4a2f78bd19f2743078b2faca8
Nat Cannon <123494773+natcannon13@users.noreply.github.com>
2023-03-10 17:22:18 -0700

    Player Card Population
    Cards are now created and populated with info.

--- lltk.js
+++ lltk.js
@@ -29,7 +29,7 @@ function playercount(operation){
     elStart.innerHTML = "Start " + playerCount;
 }
 
-function setup(nPlayers, preset){
+function setup(){
     elStart = document.getElementById("menu-start");
     elStart.classList.add("visually-hidden");
 
@@ -41,32 +41,53 @@ function setup(nPlayers, preset){
     cturn = 0;
     players = [];
     Discard = [];
-    if (preset === "default"){
-        roleList = shuffle(roleList);
-    }
-    for (let i = 0; i < nPlayers - 2; i++){
+    roleList = shuffle(roleList);
+    for (let i = 0; i < playerCount - 2; i++){
         rolesInGame.push(roleList[i]);
     }
-    rolesInGame.shuffle;
-    for(let i = 0; i < nPlayers; i++){
+    rolesInGame = shuffle(rolesInGame);
+    for(let i = 0; i < playerCount; i++){
         let p = new Player("Player " + (i + 1), rolesInGame.pop());
         draw_random(deck, p.hand);
         draw_random(deck, p.hand);
-        p.c = 2;
+        p.hand.c = 2;
         players.push(p);
     }
-    
     cardBox = document.getElementById("role-cards");
+    for(let i = 0; i < playerCount; i++){
+        let card = document.createElement('div');
+        card.setAttribute("id", "[player-" + (i + 1));
+        card.className = "card text-dark text-center"
+        let text = document.createElement('p');
+        let subtext = document.createElement('p');
+        subtext.setAttribute("id", "subtext-" + (i + 1));
+        subtext.classList.add("subtext");
+        text.innerHTML = players[i].name;
+        card.appendChild(text);
+        card.appendChild(subtext);
+        cardBox.appendChild(card);
+    }
+    updateCards();
 }
 function shuffle(list){
     let list2 = [];
     size = list.length;
     for(let i = 0; i < size; i++){
-        let r = Math.floor(Math.random() * 60);
+        let r = Math.floor(Math.random() * list.length);
         list2.push(list[r]);
-        list.remove(r); 
+        list.splice(r, 1); 
     }
     return list2;
+}
+function updateCards(){
+    for(let i = 0; i < playerCount; i++){
+        subtext = document.getElementById("subtext-" + (i + 1));
+        ph = players[currentPlayer].hand;
+        subtext.innerHTML = "Cards: " + (ph.k + ph.i + ph.p + ph.m) + " Coins: " + ph.c;
+    }
+}
+function displayHand(){
+
 }
 class Player{
     constructor(name, role){
@@ -82,18 +103,25 @@ class Player{
     }
 }
 function turn(){
+    let actionBar = document.getElementById("action-bar");
+    actionBar.setAttribute()
     let actions = 2;
-    players[currentPlayer].c += 3;
+    let drawn = false;
+    players[currentPlayer].hand.c += 3;
     function buy(){
+        if(drawn){
+            return false;
+        }
 
     }
     function buy(source){
         if (source === "deck"){
-            draw_random(deck, currentPlayer.hand);
+            draw_random(deck, players[currentPlayer].hand);
         }
         if (source === "discard"){
-            
+            add_card(players[currentPlayer].hand ,discard.pop());
         }
+        drawn = true;
     }
     function sell(){
 
@@ -104,18 +132,44 @@ function turn(){
         }
     }
     function bribe(){
+        if (players[currentPlayer].hand.c < 3){
+            return false;
+        }
 
     }
     function bribe(target, card){
+        players[currentPlayer].hand.c -= 3;
+        target.hand.c += 3;
+        if(target.hand.remove_card(card)){
+            players[currentPlayer.hand].add_card(card);
+        }
+
+    }
+    function investigate(){
 
     }
     function investigate(target, cards){
 
     }
-    function extort(target, choice){
+    function extort(){
 
     }
-    function ally(){
+    function extort(target, choice){
+        if (choice === 'card'){
+            draw_random(target.hand, players[currentPlayer].hand);
+        }
+        if (choice === 'coin'){
+            if (target.hand.c > 4){
+                target.hand.c -= 4;
+                players[currentPlayer].hand.c += 4;
+            }
+            else{
+                players[currentPlayer].hand.c += target.hand.c;
+                target.hand.c = 0;
+            }
+        }
+    }
+    /*function ally(){
 
     }
     function trade(){
@@ -123,30 +177,132 @@ function turn(){
     }
     function sabotage(){
 
-    }
+    }*/
     function seize_power(){
+
+    }
+    function seize_power(cost){
 
     }
     function seize_player(){
 
     }
+    function seize_player(target){
+
+    }
     function assassinate(){
 
     }
-    function seize_power_with_power(){
-        for(i = 0; i < discard.length; i++){
+    function assassinate(target){
 
+    }
+    function kill(target){
+
+        players.remove(currentPlayer);
+    }
+    function seize_power_with_power(){
+        for(i = 0; i < Discard.length; i++){
+            if (Discard[i] === "Knowledge"){
+                player[currentPlayer].hand.k++;
+                Discard.remove(i);
+                i--;
+            }
         }
     }
     function duel(){
         
     }
+    function duel(target){
+        if (player[currentPlayer].hand.m < target.hand.m){
+            kill(players[currentPlayer]);
+        }
+        else if (!spare()){
+            kill(target);
+            players.currentPlayer.m -= 4;
+            discard("Might");
+            discard("Might");
+            discard("Might");
+            discard("Might");
+        }
+        else{
+            players.currentPlayer.m -= 2;
+            discard("Might");
+            discard("Might");
+        }
+    }
+    function spare(){
+
+    }
     function accuse(){
 
     }
 }
+function win(time){
+    return false;
+}
 function choose_cards(){
 
+}
+function remove_card(target, card){
+
+    if (card === 'Might'){
+        if (target.m === 0){
+            return false;
+        }
+        target.m--;
+        return true;
+    }
+    if(card === 'Power'){
+        if (target.p === 0){
+            return false;
+        }
+        target.m--;
+        return true;
+    }
+    if(card === 'Knowledge'){
+        if (target.k === 0){
+            return false;
+        }
+        target.m--;
+        return true;
+    }
+    if(card === 'Influence'){
+        if (target.i === 0){
+            return false;
+        }
+        target.m--;
+        return true;
+    }
+function add_card(target, card){
+    if (card === 'Might'){
+        if (target.m === 0){
+            return false;
+        }
+        target.m++;
+        return true;
+    }
+    if(card === 'Power'){
+        if (target.p === 0){
+            return false;
+        }
+        target.m++;
+        return true;
+    }
+    if(card === 'Knowledge'){
+        if (target.k === 0){
+            return false;
+        }
+        target.m++;
+        return true;
+    }
+    if(card === 'Influence'){
+        if (target.i === 0){
+            return false;
+        }
+        target.m++;
+        return true;
+    }
+}
 }
 function discard(card){
     Discard.push(card);
